c528052e2bd9f7948524aa2fc784ac00e7a310b1
Eileen76 <inyoung.r@gmail.com>
2021-10-19 19:54:14 +0900

    기초 박스 완성

--- src/App.js
+++ src/App.js
@@ -1,43 +1,39 @@
 import React from 'react';
+export default App;
 
 function App() {
-  const styles ={
+  /*const styles ={
     height:'150px',
     width:'150px',
     backgroundColor:'pink'
-  };
+  };*/
 
-  const square = () => {
+  const Square = () => {
   return <div>
     <div className='first-row'>
-      <button id ='1'></button>
-      <button id ='2' ></button>
-      <button id ='3' ></button>
+      <button id ='1'>1</button>
+      <button id ='2'>2</button>
+      <button id ='3'>3</button>
     </div>
     
     <div className='second-row'>
-      <button id ='4' ></button>
-      <button id ='5' ></button>
-      <button id ='6' ></button>
+      <button id ='4' >4</button>
+      <button id ='5' >5</button>
+      <button id ='6' >6</button>
     </div>
 
     <div className='third-row'>
-      <button id ='7' ></button>
-      <button id ='8' ></button>
-      <button id ='9' ></button>
+      <button id ='7' >7</button>
+      <button id ='8' >8</button>
+      <button id ='9' >9</button>
     </div>
 
-  </div>;
+  </div>;}
     
-
   return (
-    <div className="App">
-      
-      
-      {square}
-      
+    <div>
+      <Square />
     </div>
   );
-}
 
-export default App;
+}
