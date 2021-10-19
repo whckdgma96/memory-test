a5f08f690e08381f5042f0315a7a4a5eb85a8e11
Eileen76 <inyoung.r@gmail.com>
2021-10-19 19:49:41 +0900

    box만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+  const styles ={
+    height:'150px',
+    width:'150px',
+    backgroundColor:'pink'
+  };
+
+  const square = () => {
+  return <div>
+    <div className='first-row'>
+      <button id ='1'></button>
+      <button id ='2' ></button>
+      <button id ='3' ></button>
+    </div>
+    
+    <div className='second-row'>
+      <button id ='4' ></button>
+      <button id ='5' ></button>
+      <button id ='6' ></button>
+    </div>
+
+    <div className='third-row'>
+      <button id ='7' ></button>
+      <button id ='8' ></button>
+      <button id ='9' ></button>
+    </div>
+
+  </div>;
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      
+      {square}
+      
     </div>
   );
 }
